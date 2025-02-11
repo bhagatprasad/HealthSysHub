b5d719aa623e709405562752503783a87f29f35b
Durga Prasad Bgahuthu <dbaghuthu@betalen.in>
2025-02-11 21:58:45 +0530

    Authentication implemented both sides

---
+++ HealthSysHub.Web.API/Controllers/AccountController.cs
@@ -0,0 +1,53 @@
+﻿using HealthSysHub.Web.Managers;
+using HealthSysHub.Web.Utility.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HealthSysHub.Web.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly IAuthenticationManager _authenticationManager;
+        public AccountController(IAuthenticationManager authenticationManager)
+        {
+            _authenticationManager = authenticationManager;
+        }
+
+        [HttpPost]
+        [Route("AuthenticateUserAsync")]
+        public async Task<IActionResult> AuthenticateUserAsync(UserAuthentication authentication)
+        {
+            try
+            {
+                var response = await _authenticationManager.AuthenticateUserAsync(authentication);
+
+
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+        [HttpPost]
+        [Route("GenarateUserClaimsAsync")]
+        public async Task<IActionResult> GenarateUserClaimsAsync(AuthResponse authentication)
+        {
+            try
+            {
+                var response = await _authenticationManager.GenarateUserClaimsAsync(authentication);
+
+
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+
+        }
+    }
+}
