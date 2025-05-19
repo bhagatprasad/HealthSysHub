f5138aed02a3292af5b6cb08c0b108583216fb10
PUNEM NAVEEN KUMAR <npunem@betalen.in>
2025-05-19 19:53:37 +0530

    commit

--- HealthSysHub.Web.Pharmacy.UI/src/app/medicines/medicines-list.component.ts
+++ HealthSysHub.Web.Pharmacy.UI/src/app/medicines/medicines-list.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PharmacyMedicineService } from '../services/pharmacy-medicine-service';
+import { PharmacyMedicine } from '../models/pharmacymedicine';
+import { IApplicationUser } from '../models/applicationuser';
+import { NotificationService } from '../services/notification.service';
 
 @Component({
   selector: 'app-medicines-list',
@@ -6,6 +10,36 @@ import { Component } from '@angular/core';
   templateUrl: './medicines-list.component.html',
   styleUrl: './medicines-list.component.css'
 })
-export class MedicinesListComponent {
+export class MedicinesListComponent implements OnInit {
+
+  medicines: PharmacyMedicine[] = [];
+  applicationUser: IApplicationUser = {};
+
+  constructor(private pharmacyMedicineService: PharmacyMedicineService, private notificationService: NotificationService) { }
+
+  ngOnInit() {
+    var appUser = localStorage.getItem("ApplicationUser");
+    if (appUser) {
+      this.applicationUser = JSON.parse(appUser);
+      this.GetPharmacyMedicineByPharmacyAsync(this.applicationUser.pharmacyId);
+    }
+  }
+
+  GetPharmacyMedicineByPharmacyAsync(pharmacyId?: string) {
+    if (pharmacyId) {
+      this.pharmacyMedicineService.GetPharmacyMedicineByPharmacyAsync(pharmacyId).subscribe({
+        next: (medicines) => this.handlePharmacyMedicineSuccess(medicines),
+        error: (error) => this.handleAuthError(error)
+      })
+    }
+  }
+  private handlePharmacyMedicineSuccess(medicines: PharmacyMedicine[]): void {
+    this.medicines = medicines;
+  }
+  private handleAuthError(error: any): void {
+    console.error('Authentication error:', error);
+    const errorMessage = error?.error?.message || error.message || 'Login failed';
+    this.notificationService.showError(errorMessage);
+  }
 
 }
