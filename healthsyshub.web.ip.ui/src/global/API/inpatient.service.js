a5f28fbbd2731b838289dc7fa7f4a91fbdbf2ed5
bhagatprasad <bhagatprasad8@yahoo.com>
2025-08-25 15:51:34 +0530

    commit

--- healthsyshub.web.ip.ui/src/global/API/inpatient.service.js
+++ healthsyshub.web.ip.ui/src/global/API/inpatient.service.js
@@ -1,14 +1,46 @@
 import { send } from '@/global/API/api.service'
 import { endpoints } from '@/environment'
 
-
 export default {
-    async GetInPatientsAsync(currentHospitalId) {
-        const url = `${endpoints.UrlConstants.GetInPatientsAsync}/${currentHospitalId}`;
+    // Get inpatients by hospital ID
+    async GetInPatientsAsync(hospitalId) {
+        const url = `${endpoints.UrlConstants.GetInPatientsAsync}/${hospitalId}`;
         const response = await send("GET", url);
         return response;
     },
-    GetActiveInPatientsAsync() {
-        console.log("GetActiveInPatientsAsync")
+
+    // Get active inpatients
+    async GetActiveInPatientsAsync() {
+        const url = endpoints.UrlConstants.GetActiveInPatientsAsync;
+        const response = await send("GET", url);
+        return response;
+    },
+
+    // Get inpatient by ID
+    async GetInPatientByIdAsync(inpatientId) {
+        const url = `${endpoints.UrlConstants.GetInPatientByIdAsync}/${inpatientId}`;
+        const response = await send("GET", url);
+        return response;
+    },
+
+    // Create or update inpatient
+    async InsertOrUpdateInpatientAsync(inpatient) {
+        const url = endpoints.UrlConstants.InsertOrUpdateInpatientAsync;
+        const response = await send("POST", url, inpatient);
+        return response;
+    },
+
+    // Delete inpatient
+    async DeleteInpatientAsync(inpatientId) {
+        const url = `${endpoints.UrlConstants.DeleteInpatientAsync}/${inpatientId}`;
+        const response = await send("DELETE", url);
+        return response;
+    },
+
+    // Get inpatients by status
+    async GetInpatientsByStatusAsync(status) {
+        const url = `${endpoints.UrlConstants.GetInpatientsByStatusAsync}/${encodeURIComponent(status)}`;
+        const response = await send("GET", url);
+        return response;
     }
 }
